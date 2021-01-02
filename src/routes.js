ba00702587bf6e88e393bb3627963f36dcc1f61a
Irwin Herridge <irwinherridge3@gmail.com>
2021-01-02 17:22:22 +0200

    Added a Home state with a link on the home component that will open the categories state... Tomorrow is another day...

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+(function () {
+
+angular.module('MenuApp',['ui.router']);
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to tab 1 if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // Set up UI states
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'src/menuapp/templates/home.menuapp.template.html'
+    })
+
+    .state('categories', {
+      url: '/categories',
+      templateUrl: 'src/menuapp/templates/categories.template.html'
+    })
+
+    .state('tab1', {
+      url: '/tab1',
+      templateUrl: 'src/tab1.html'
+    })
+
+    .state('tab2', {
+      url: '/tab2',
+      templateUrl: 'src/tab2.html'
+    });
+}
+
+
+})();
